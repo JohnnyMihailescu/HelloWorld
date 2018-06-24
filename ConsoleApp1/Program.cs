640a4f5b6bd0b84ce13233c2ed89289e1b3c08c2
JohnnyMihailescu <johnnym151@gmail.com>
2018-06-24 17:08:18 -0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+        
+            Console.WriteLine("Say your name");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"/nHello, {name} on {date:d} at {date:t}");
+            Console.WriteLine("Press Enter to Exit");
+            Console.ReadKey(true);
+        }
+    }
+}
